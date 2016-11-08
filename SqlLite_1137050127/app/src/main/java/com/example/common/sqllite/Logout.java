4b39c79825996ff07b656541d62a32e9b32d5ca5
dwikiolajuwon <mudiseo@student.uinsgd.ac.id>
2016-11-08 21:56:39 +0700

    Tugas 4

---
+++ SqlLite_1137050127/app/src/main/java/com/example/common/sqllite/Logout.java
@@ -0,0 +1,16 @@
+package com.example.common.sqllite;
+
+/**
+ * Created by MDSO on 11/8/2016.
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Logout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
